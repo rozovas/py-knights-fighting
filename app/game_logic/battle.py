6faf17ceb5f5ccfe8bb8bb4589c9019f2d5f9c6b
Svitlana Rozova <svitlana.rozova@gnail.com>
2024-01-21 11:53:55 +0200

    removed unnecesary class, added its logic to knight class

--- app/game_logic/battle.py
+++ app/game_logic/battle.py
@@ -9,10 +9,10 @@ class Battle:
         self.players = [self.player1, self.player2]
 
     def play_round(self) -> None:
-        self.player1.hp -= self.player2.power - self.player1.protection
-        self.player2.hp -= self.player1.power - self.player2.protection
-
-        # check if someone fell in battle
         for player in self.players:
+            another_player = self.player2 if player == self.player1 \
+                else self.player2
+            player.hp -= another_player.power - player.protection
+            # check if someone fell in battle
             if player.hp <= 0:
                 player.hp = 0
--- app/game_logic/knight.py
+++ app/game_logic/knight.py
@@ -1,6 +1,12 @@
 class Knight:
 
-    def __init__(self, name, power, hp, armour, weapon, potion) -> None:
+    def __init__(self,
+                 name: str,
+                 power: int,
+                 hp: int,
+                 armour: list[dict],
+                 weapon: dict,
+                 potion: dict | None) -> None:
         self.name = name
         self.power = power
         self.hp = hp
@@ -9,5 +15,19 @@ class Knight:
         self.potion = potion
         self.protection = 0
 
+    def apply_armour(self) -> None:
+        for armour in self.armour:
+            self.protection += armour["protection"]
 
+    def apply_weapon(self) -> None:
+        self.power += self.weapon["power"]
 
+    def apply_potion(self) -> None:
+        if self.potion is not None:
+            for item in self.potion["effect"]:
+                self.item += self.potion["effect"][item]
+
+    def prepare_knight_for_battle(self) -> None:
+        self.apply_armour()
+        self.apply_weapon()
+        self.apply_potion()
